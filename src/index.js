5adfc3ed6cd95d818aaaed13e4b97490d4b6c30a
Will Jones <j031438j@student.staffs.ac.uk>
2024-05-24 20:46:11 +0100

    class App

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import { HashRouter } from "react-router-dom";
+import { HashRouter, Route, Link } from "react-router-dom";
 
 const IndexPage = () => <h3>Home Page</h3>;
 
@@ -16,15 +16,23 @@ function App() {
 }
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <HashRouter basename="/">
-            <section className="App">
-                <Router>
-                    <Link to="/">Home</Link>
-                    <Route exact path="/" component={IndexPage} />
-                </Router>
-            </section>
-        </HashRouter>
-    </React.StrictMode>
-)
+class App extends Component {
+    render() {
+        return (
+            ReactDOM.createRoot(document.getElementById('root')).render(
+                <React.StrictMode>
+                    <HashRouter basename="/">
+                        <section className="App">
+                            <Router>
+                                <Link to="/">Home</Link>
+                                <Route exact path="/" component={IndexPage} />
+                            </Router>
+                        </section>
+                    </HashRouter>
+                </React.StrictMode>
+            )
+        )
+    }
+}
+
+export default App;
