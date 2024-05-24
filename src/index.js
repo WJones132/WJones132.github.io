93ed9d9fca751432334789bc1ded6734eecdcd70
Will Jones <j031438j@student.staffs.ac.uk>
2024-05-24 20:42:18 +0100

    modified again

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { HashRouter } from "react-router-dom";
 
-const IndexPage = () => {
-    return <h3>Home Page</h3>;
-};
+const IndexPage = () => <h3>Home Page</h3>;
 
 function App() {
     return (
@@ -18,14 +16,15 @@ function App() {
 }
 
 
-const rootElement = ReactDOM.createRoot(document.getElementById('root'));
-rootElement.render(
-    <HashRouter>
-        <section className="App">
-            <Router>
-                <Link to="/">Home</Link>
-                <Route exact path="/" element={<IndexPage />} />
-            </Router>
-        </section>
-    </HashRouter>
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <HashRouter basename="/">
+            <section className="App">
+                <Router>
+                    <Link to="/">Home</Link>
+                    <Route exact path="/" component={IndexPage} />
+                </Router>
+            </section>
+        </HashRouter>
+    </React.StrictMode>
 )
